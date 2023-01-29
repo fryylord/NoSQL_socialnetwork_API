733f27069399a474066b57d1341064c3f98fc67c
fryylord <rob@mchays.com>
2023-01-29 13:49:37 -0600

    api index.js created, empty files for thoughts/users

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-// app.use(require("./routes"));
+app.use(require("./routes"));
 
 db.once("open", () => {
   app.listen(PORT, () => {
