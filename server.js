a349340759fefd76a789fda7529ce5774488e290
fryylord <rob@mchays.com>
2023-01-29 13:24:00 -0600

    refactored a bit of server/connection

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const db = require("./config/connection");
-const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
+app.use(express.static("public"));
+app.use(require("./routes"));
 
 db.once("open", () => {
   app.listen(PORT, () => {
