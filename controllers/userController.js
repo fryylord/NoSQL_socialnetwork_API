934e15bc3712b2460e5704f20adb91e07c3a0c4a
fryylord <rob@mchays.com>
2023-02-01 16:07:19 -0600

    added friends to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -31,6 +31,63 @@ getOneUser({ params }, res) {
         console.log(err);
         res.sendStatus(500);
     });
-}
+},
 
-}
+createUser(req, res) {
+    User.create(req.body)
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => res.status(500).json(err));
+},
+
+updateUser(req, res) {
+    User.findOneAndUpdate({ _id: req.params.userId }, req.body, { new: true })
+    .then((dbUserData) => {
+        if (!dbUserData) {res.status(404).json({ message: 'No user found!' }); 
+        return;}
+        res.json(dbUserData);
+    })
+    .catch((err) => res.status(500).json(err));
+},
+
+deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+    .then((dbUserData) => {
+        if (!dbUserData) {res.status(404).json({ message: 'No user found!' }); 
+        return;}
+        res.json(dbUserData);
+    })
+    .catch((err) => res.status(500).json(err));
+},
+
+addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $push: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+
+  deleteFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status5400).json(err);
+  }
+};
